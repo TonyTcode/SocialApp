8a1819b5a2894abd5de73e17af8b49823d45be6e
Antonio Tobar <antonio.tobar.dev@gmail.com>
2019-09-24 09:38:51 -0500

    user reg

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
+connectDB();
+
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('T-800 deployed'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
 
 const PORT = process.env.PORT || 5000;
 
