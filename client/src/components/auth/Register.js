3dcf9da5dfad1eacf0bc949871dc73cbf4b82d54
Antonio Tobar <antonio.tobar.dev@gmail.com>
2019-09-26 17:50:17 -0500

    alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -13,12 +17,12 @@ const Register = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Password does not match');
+      setAlert('Password does not match', 'danger');
     } else {
-      console.log(formData);
+      console.log('Success');
     }
   };
 
@@ -76,10 +80,16 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
 };
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
 
-export default Register;
+export default connect(
+  null,
+  { setAlert }
+)(Register);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,9 +5,7 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <Link to='/'>
-          <i className='fas fa-code'></i> GamerConnector
-        </Link>
+        <Link to='/'>GamerConnector</Link>
       </h1>
       <ul>
         <li>
